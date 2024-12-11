476a424d735a5963fb4b501b833279e311f2a5c1
patricklimax <patricklimax@outlook.com.br>
2024-12-11 00:52:37 -0300

    chore: adding the UserManual Component to the App Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Mapa } from './components/Mapa';
+import { UserManual } from './components/UserManual';
 
 function App() {
 	return (
 		<section>
 			<Mapa />
+			<UserManual />
 		</section>
 	);
 }
